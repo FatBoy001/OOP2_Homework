b5f3b221fb47e231a449d47fba02b0b2ff858f6b
FatBoy001_jpg <51392131+FatBoy001@users.noreply.github.com>
2022-09-25 23:20:12 +0800

    Add files via upload

--- Week2/HashMap/hashmap.java
+++ Week2/HashMap/hashmap.java
@@ -1,27 +1,28 @@
 import java.util.HashMap;
 import java.util.Scanner;
 public class hashmap{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        HashMap<Integer,String> hm = new HashMap<Integer,String>();
-        hm.put(1,"January");
-        hm.put(2,"February ");
-        hm.put(3,"March");
-        hm.put(4,"April");
-        hm.put(5,"May");
-        hm.put(6,"June");
-        hm.put(7,"July");
-        hm.put(8,"August");
-        hm.put(9,"September");
-        hm.put(10,"October");
-        hm.put(11,"November");
-        hm.put(12,"December");
-        while(true){
+    public static void main(String[] args){  
+        try(Scanner input = new Scanner(System.in)){
+            HashMap<Integer,String> hm = new HashMap<Integer,String>();
+            hm.put(1,"January");
+            hm.put(2,"February ");
+            hm.put(3,"March");
+            hm.put(4,"April");
+            hm.put(5,"May");
+            hm.put(6,"June");
+            hm.put(7,"July");
+            hm.put(8,"August");
+            hm.put(9,"September");
+            hm.put(10,"October");
+            hm.put(11,"November");
+            hm.put(12,"December");
+    
             System.out.print("請輸入1～12？");
             int value = input.nextInt();
-            if(value>12||value<1){
+            while(value>12||value<1){
                 System.out.println("範圍錯誤！");
-                continue;
+                System.out.print("請輸入1～12？");
+                value = input.nextInt();
             }
             System.out.printf("第%d月的英文單字為 %s%n",value,hm.get(value));
         }
